660c5b499be39bbda8f6e819c7818624fbdaaf69
srijanpal07 <123281965+srijanpal07@users.noreply.github.com>
2023-12-07 03:13:16 -0600

    changes

--- scripts/stream_data_collect.py
+++ scripts/stream_data_collect.py
@@ -1,36 +1,36 @@
-#!/usr/local/bin/python3.10
+#!/usr/bin/python3
+# license removed for brevity
 
-import msgpackrpc
+from operator import truediv
+from re import sub
 import rospy
-import airsim
-import numpy as np
+from rospy.client import init_node
 from sensor_msgs.msg import Image
+from vision_msgs.msg import Detection2D
+from sensor_msgs.msg import TimeReference
+import numpy as np
 import cv2
+#from cv2 import cvShowImage
 import os, re
-from pathlib import Path
+# import PySpin
 import sys, datetime
+import argparse
+from pathlib import Path
+import time
+import torch
 from std_msgs.msg import String
-HOST = '127.0.0.1' # Standard loopback interface address (localhost)
-from platform import uname
-if 'linux' in uname().system.lower() and 'microsoft' in uname().release.lower(): # In WSL2
-    if 'WSL_HOST_IP' in os.environ:
-        HOST = os.environ['WSL_HOST_IP']
-print("Using WSL2 Host IP address: ", HOST)
-# client = airSim.multiiRotorClient(ip=HOST)
-client = airsim.MultirotorClient(ip=HOST)
-client.confirmConnection()
-client.enableApiControl(True)
+from sensor_msgs.msg import TimeReference
 
 
 global view_img, save_img, save_format
 view_img = True
-save_img = False
+save_img = True
 save_format = '.png'
 
 tmp = datetime.datetime.now()
 stamp = ("%02d-%02d-%02d" % 
     (tmp.year, tmp.month, tmp.day))
-maindir = Path('./SavedData')
+maindir = Path('~/Data')
 runs_today = list(maindir.glob('*%s*_stream' % stamp))
 if runs_today:
     runs_today = [str(name) for name in runs_today]
@@ -44,6 +44,10 @@ savedir = maindir.joinpath('%s_run%02d_stream' % (stamp,new_run_num))
 os.makedirs(savedir) 
 
 
+def time_callback(gpstime):
+    global gps_t
+    gps_t = float(gpstime.time_ref.to_sec())
+
 
 def saveimagecallback(smoketrack):
     global view_img, save_img, save_format
@@ -52,32 +56,21 @@ def saveimagecallback(smoketrack):
         print('Saving Images')
 
 
-def init_data_collection_node():
-    global view_img, save_img, save_format
-    rospy.init_node("droneCam_datacollect")
-    rospy.Subscriber('smoketrack', String, saveimagecallback)
-    seq = 1
-    img = Image()
+def imagecallback(img):
+    global pub,box,video,timelog
+    global imgsz, model, device, names
+    # box = Detection2D()
+    img_numpy = np.frombuffer(img.data,dtype=np.uint8).reshape(img.height,img.width,-1)
 
-    while not rospy.is_shutdown():
-        responses = client.simGetImages([airsim.ImageRequest("front_center", airsim.ImageType.Scene, False, False)])
-        response = responses[0]
-
-        img1d = np.frombuffer(response.image_data_uint8, dtype=np.uint8) 
-
-        # reshape array to 4 channel image array H X W X 4
-        img_rgb = img1d.reshape(response.height, response.width, 3)
-
-        img.header.seq = seq
-        img.header.stamp = rospy.Time.now()
-        seq += 1
-        img.height,img.width = response.height, response.width
-        img.data = img_rgb.flatten().tolist()
-
-        rospy.sleep(0.0001)
-
+    if rospy.Time.now() - img.header.stamp > rospy.Duration(0.5):
+        print("DetectionNode: dropping old image from detection\n")
+        # text_to_image = 'skipped'
+        return
+    else:
+        print('DetectionNode: Running detection inference')        
+        
         if view_img:
-            result = img_rgb
+            result = img_numpy
             scale_percent = 25 # percent of original size
             width = int(result.shape[1] * scale_percent / 100)
             height = int(result.shape[0] * scale_percent / 100)
@@ -92,7 +85,23 @@ def init_data_collection_node():
             savenum = img.header.seq
             if save_format=='.png':
                 filename = str(savedir.joinpath(f'stream{tmp.month:02d}{tmp.day:02d}{tmp.year}_{savenum:06d}.png'))
-                cv2.imwrite(filename,img_rgb)
+                cv2.imwrite(filename,img_numpy)
+    # viewing/saving images
+    savenum=img.header.seq
+
+
+
+def init_data_collection_node():
+    global view_img, save_img, save_format
+    rospy.init_node("droneCam_datacollect")
+    rospy.Subscriber('smoketrack', String, saveimagecallback)
+    rospy.Subscriber('/camera/image', Image, imagecallback)
+    rospy.Subscriber('mavros/time_reference',TimeReference,time_callback)
+    seq = 1
+    img = Image()
+
+    while not rospy.is_shutdown():
+        rospy.sleep(0.000)
 
 
 if __name__ == '__main__':
